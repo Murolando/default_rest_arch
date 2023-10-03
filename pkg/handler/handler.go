62bcd9493a0a392f50821055d598d40f82a41704
Murolando <60064242+Murolando@users.noreply.github.com>
2023-10-03 22:04:43 +0300

    add swagger

--- pkg/handler/handler.go
+++ pkg/handler/handler.go
@@ -3,6 +3,10 @@ package handler
 import (
 	"github.com/Murolando/default_rest_arch/pkg/service"
 	"github.com/gin-gonic/gin"
+
+	_ "github.com/Murolando/default_rest_arch/docs"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
 type Handler struct {
@@ -22,13 +26,14 @@ func (h *Handler) InitRoutes() *gin.Engine {
 	// router.Static("/storage","./storage")
 	router.MaxMultipartMemory = 8 << 20 // 8 MiB
 	// router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
-	api:= router.Group("/api")
+	api := router.Group("/api")
 	{
+		api.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 		auth := api.Group("/auth")
 		{
-			auth.POST("/sign-up",h.signUp)
-			auth.POST("/sign-in",h.signIn)
-			auth.GET("/refresh/:refresh",h.newRefresh)
+			auth.POST("/sign-up", h.signUp)
+			auth.POST("/sign-in", h.signIn)
+			auth.GET("/refresh/:refresh", h.newRefresh)
 		}
 	}
 	return router
