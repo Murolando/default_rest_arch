f0c01db7349259115f7c34f074d6f0298798063e
Murolando <60064242+Murolando@users.noreply.github.com>
2023-12-09 02:01:49 +0300

    fix

--- pkg/handler/middleware.go
+++ pkg/handler/middleware.go
@@ -1,26 +1,24 @@
 package handler
 
 
-func (h *Handler) userIdentity(next echo.HandlerFunc) echo.HandlerFunc {
-	return func(c echo.Context) error {
-		header := c.Request().Header.Get("Authorization")
-		if !strings.HasPrefix(header, "Bearer ") {
-			// h.HTTPErrorHandler(errNotAuthorized, c)
-			return errNotAuthorized
-		}
-
-		headerParts := strings.Split(header, " ")
-		if len(headerParts) != 2 {
-			// h.HTTPErrorHandler(errNotAuthorized, c)
-			return errNotAuthorized
-		}
-		tokenString := headerParts[1]
-		userId, err := h.service.Auth.ParseToken(tokenString)
-		if err != nil || userId == 0 {
-			// h.HTTPErrorHandler(errIvalidJWTToken, c)
-			return errIvalidJWTToken
-		}
-		c.Set("userId", userId)
-		return next(c)
+func (h *Handler) userIdentity(c *gin.Context) {
+	header := c.GetHeader(authHeader)
+	if header == "" {
+		newErrorResponse(c, http.StatusUnauthorized, "empty header")
+		return
 	}
+
+	headerParts := strings.Split(header, " ")
+	if len(headerParts) != 2 {
+		// fmt.Println(headerParts)
+		newErrorResponse(c, http.StatusUnauthorized, "invalid auth header")
+		return
+	}
+
+	userId, err := h.service.Auth.ParseToken(headerParts[1])
+	if err != nil || userId == 0 {
+		newErrorResponse(c, http.StatusUnauthorized, err.Error())
+		return
+	}
+	c.Set("userId", userId)
 }
